7bf9d9976bb44b149b2e6d3943863a92ca3ae583
zhanghn <notre1024@163.com>
2026-04-28 23:13:02 +0800

    fix(app): 集成 Vuex 状态管理
    - 引入 Vuex 依赖并注册到 Vue 应用
    - 导入 store 模块并配置到根实例
    - 在 Vue 构造函数中添加 store 选项
    - 保持原有的 router 配置不变

--- rain/src/main.js
+++ rain/src/main.js
@@ -4,10 +4,13 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import router from "@/router";
+import store from "@/store";
 import * as echarts from "echarts";
 
 Vue.use(VueRouter)
+Vue.use(Vuex)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
@@ -18,5 +21,6 @@ new Vue({
     Vue.prototype.$bus = this
     Vue.prototype.$echarts = echarts;
   },
-  router
+  router,
+  store
 }).$mount('#app')
